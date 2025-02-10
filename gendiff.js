c5ac36f601a3959e52c92ccb3421719d7c430e06
Vladimir Perov <per0w@yandex.ru>
2025-02-10 21:19:49 +0400

    step 3, added parse file

--- gendiff.js
+++ gendiff.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { program } from 'commander';
 
-const program = new Command();
+import parse from './parse.js';
+
+const command = (filepath1, filepath2) => {
+  const file1 = parse(filepath1);
+
+  const file2 = parse(filepath2);
+
+  console.log(file1, file2);
+};
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .arguments('<filepath1>, <filepath2>')
-  .option('-f, --format [type]', 'output format');
-
-program.parse();
+  .option('-f, --format [type]', 'output format')
+  .action(command)
+  .parse(process.argv);
---
+++ parse.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import { readFileSync } from 'fs';
+
+import getFileExtension from './utils.js';
+
+const parse = (fileName) => {
+  const extension = getFileExtension(fileName);
+
+  switch (extension) {
+    case 'json': {
+      return JSON.parse(readFileSync(path.resolve(fileName)));
+    }
+
+    default: {
+      return null;
+    }
+  }
+};
+
+export default parse;
