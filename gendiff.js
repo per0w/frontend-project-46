ded2e712507d873e99ce8ccaa6af72db727fce75
Vladimir Perov <per0w@yandex.ru>
2025-02-10 17:29:51 +0400

    added new arguments

--- gendiff.js
+++ gendiff.js
@@ -1,9 +1,13 @@
+#!/usr/bin/env node
+
 import { Command } from 'commander';
 
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .arguments('<filepath1>, <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
